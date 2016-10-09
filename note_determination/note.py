4d4dcf58a3037f0976fffddf4fce656ae8d142ab
Azra Ismail <aismail1997@gmail.com>
2016-10-08 21:47:50 -0400

    cleaned up stuff

--- note_determination/note.py
+++ note_determination/note.py
@@ -1,13 +1,8 @@
-#import wolframalpha
-#app_id = "4E8KVK-8P9TTRTG27"
-#client = wolframalpha.Client(app_id)
-
 import numpy.fft as fft
 
 # map of frequencies to notes
 def get_note(data):
     notes_map = {'C': 261.63 ,'C#': 277.18,'D': 293.66, 'D#': 311.13, 'E': 329.63, 'F': 349.23, 'F#': 369.99, 'G': 392, 'G#': 415.3, 'A': 440, 'A#': 466.16,'B': 493.88}
-    #res = client.query('Fourier' + array + '-mean' + array)
     spectrum = fft.fft(data) # doing an fft on the data to get frequency spectrum
     freq = fft.fftfreq(len(spectrum)) # get the frequencies
     currentfreq = max(abs(spectrum))
